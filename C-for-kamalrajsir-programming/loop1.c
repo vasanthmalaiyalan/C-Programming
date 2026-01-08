3a5724b7cc5b33f793e24cdbd250af2262955635
saitama <viaduct-bot@users.noreply.github.com>
2026-01-08 22:31:57 +0530

    Initial commit

---
+++ C-for-kamalrajsir-programming/loop1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    do{
+        printf("Iam from do..while");
+    }while(0);
+
+    //entry condition verfication
+    while(0)
+    {
+        printf("Iam from while");
+    }
+}
