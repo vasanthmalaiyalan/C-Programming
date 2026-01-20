87539a2139bb736283aa9af13dccc97bcaaab50c
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-01-20 08:02:21 +0000

    The C practice

--- Let-us-C-Yashwant/logic operator/logiope3.c
+++ Let-us-C-Yashwant/logic operator/logiope3.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i=-3,j=3;
-    if(!i+!j*1){
+    if(!i+!j*1){ //non-zero values are always consider true ok so if(0+0*1 = 0)else block executed
        printf("\nhey iam C");
     }
     else{
