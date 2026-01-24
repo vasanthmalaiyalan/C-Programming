29575e11aad0aca0de8845256fd0e336080b3f1e
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-01-24 16:38:28 +0000

    The C

---
+++ Let-us-C-Yashwant/mysystemdatatype.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("int : %lu bytes\n",sizeof(int));
+    printf("long : %lu bytes\n",sizeof(long));
+    printf("float : %lu bytes\n",sizeof(float));
+    printf("double : %lu bytes\n",sizeof(double));
+}
--- Let-us-C-Yashwant/pointers/pointer3.c
+++ Let-us-C-Yashwant/pointers/pointer3.c
@@ -10,8 +10,7 @@ int main()
     printf("Address of *k (*This is pointer) = %p\n",(void*)*k); //point to address that address only get in to value 
     printf("Address of &j  = %p\n",(void*)&j);
     printf("Address of &k = %p\n",(void*)&k);
-    printf("value of k = %p\n",(void*)k);
-    printf("value of j = %p\n",(void*)j);
+    printf("Address of k = %p\n",(void*)k);
     printf("value of i = %d\n",i);
     printf("value of *j = %d\n",*j);
     printf("value of **k (This means go to *k -> inside *k (&i) address store in value like 0.x456 then **k means go to -> that value represent address of (&i) next you will get that value = %d\n",**k);
