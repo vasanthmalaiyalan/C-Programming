21cfb6fecc6d73889054d4b15fea4847c2c93ab9
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-01-27 18:23:42 +0000

    The C

--- Let-us-C-Yashwant/array/2darray.c
+++ Let-us-C-Yashwant/array/2darray.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int stud[4][2];
+    int stud[4][2]; // 4 students, each has (roll, marks)
     int i,j;
 
     for(i=0;i<=3;i++)
     {
-        printf("Enter roll no and marks: ");
-        scanf("%d %d\n",&stud[i][0],&stud[i][1]);
+        printf("Enter roll number and marks for student %d:",i+1);
+        scanf("%d %d",&stud[i][0],&stud[i][1]);
     }
+        printf("\n--- Student Data ---\n");
     for(i=0;i<=3;i++)
-        printf("%d %d\n",stud[i][0],stud[i][1]);
+        printf("Student %d -> Roll: %d , Marks: %d\n",i+1,stud[i][0],stud[i][1]);
 }
