03cce8f53341021649ef2128f7ee205437d8cb8c
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-01-29 18:03:28 +0000

    The C

---
+++ Let-us-C-Yashwant/array/2dpointarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//usage of pointer to an array
+int main()
+{
+    int s[5][2] = {
+        {1234,56},
+        {3456,12},
+        {2345,23},
+        {2345,44}
+    };
+    int (*p)[2]; 
+    int i,j,*pint;
+
+    for(i=0;i<=3;i++)
+    {
+        p = &s[i];
+        pint = p;
+        printf("\n");
+        for(j=0;j<=1;j++)
+            printf("%d ",*(pint+j));
+    }
+}
