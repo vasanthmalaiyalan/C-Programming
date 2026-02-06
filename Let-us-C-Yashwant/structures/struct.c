b9d66369a5404bccc882e10a654e9be221767e1f
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-02-06 08:34:48 +0000

    structure

--- Let-us-C-Yashwant/structures/struct.c
+++ Let-us-C-Yashwant/structures/struct.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    char name[3];
+    char name[3];//expression decay default because array just use like scanf call like -> name,default array+decay  alway first element address     
     float price[3];
     int pages[3],i;
 
@@ -15,6 +15,6 @@ int main()
     printf("And this is what you enter: \n");
     for(i=0;i<3;i++)
     {
-        printf(" %c\t %f\t %d\n",name[i],price[i],pages[i]);
+        printf(" %c\t %f\t %d\n",name[i],price[i],pages[i]);//this si value store array like this one 
     }
 }
---
+++ Let-us-C-Yashwant/structures/struct1.c
@@ -0,0 +1,22 @@
+ #include<stdio.h>
+ 
+ int main()
+ {
+    struct book
+    {
+        char name;
+        float price;
+        int pages;
+    };
+    struct book b1,b2,b3;
+
+    printf("Enter names ,price & no.of pages of books\n");
+    scanf(" %c %f %d",&b1.name,&b1.price,&b1.pages);
+    scanf(" %c %f %d",&b2.name,&b2.price,&b2.pages);
+    scanf(" %c %f %d",&b3.name,&b3.price,&b3.pages);
+
+    printf("And you this is what you entered");
+    printf(" %c %f %d\n",b1.name,b1.price,b1.pages);
+    printf(" %c %f %d\n",b2.name,b2.price,b2.pages);
+    printf(" %c %f %d\n",b3.name,b3.price,b3.pages);
+ }
